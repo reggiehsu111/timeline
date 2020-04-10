b2e916ce37df6ba96a12d093c6aeae180e9852e5
Reggie Hsu <reggiehsu111@gmail.com>
2020-04-10 14:13:19 +0800

    modified activity table

--- src/ActivityTable.js
+++ src/ActivityTable.js
@@ -25,13 +25,16 @@ class ActivityTable extends React.Component{
         var blocks = [];
         var id, name;
         for (var i=0; i<this.state.activityList.length; i++){
-            blocks.push(
-            <tr>
-                <td>{this.state.activityList[i].diff_day}</td>
-                <td>{this.state.activityList[i].time}</td>
-                {this.insertEvents(this.state.activityList[i].event)}
-            </tr>
-            )
+            for (var j=0; j<this.state.activityList[i].event.length; j++){
+                blocks.push(
+                    <tr>
+                        <td style={{width:"30%"}}>{this.state.activityList[i].diff_day}</td>
+                        <td>{this.state.activityList[i].date}</td>
+                        <td>{this.state.activityList[i].event[j]}</td>
+                    </tr>
+                    )
+            }
+            
         }
         return blocks;
     }
@@ -43,7 +46,7 @@ class ActivityTable extends React.Component{
     		return (
     			<table id="activity_table">
 	              <tr>
-	                <th>距離確診天數</th>
+	                <th>距離發病天數</th>
 	                <th>日期</th> 
 	                <th>事件</th> 
 	              </tr>
@@ -53,6 +56,7 @@ class ActivityTable extends React.Component{
     	}
     }
     render(){
+        console.log(this.state.activityList);
         return (
           this.insertTable()
         )
